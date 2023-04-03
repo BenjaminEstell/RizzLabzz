d5c19fc5ccdb90c39936532fd106637eb486e8d7
Ubuntu <ubuntu@rizzlabzzfreee2.us-east1-b.c.eecs441rizzlabzz.internal>
2023-04-03 13:02:41 +0000

    Adding ocr.py

---
+++ server/app/ocr.py
@@ -0,0 +1,19 @@
+from google.cloud import vision
+import io
+
+
+def detect_text(content):
+    client = vision.ImageAnnotatorClient()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+    else:
+        return texts
