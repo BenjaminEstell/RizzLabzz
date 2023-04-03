da0b7de01628c28167e6c56d1986416794f71ad4
ananyasharma20 <anasharm@umich.edu>
2023-04-02 20:53:22 -0400

    added machine view class
    need to fix failed get request error and upload image error

--- app/src/main/java/edu/umich/mlyao/gymbrofe/MachineActivity.kt
+++ app/src/main/java/edu/umich/mlyao/gymbrofe/MachineActivity.kt
@@ -5,11 +5,16 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import edu.umich.mlyao.gymbrofe.databinding.MachineCardBinding
 import okhttp3.*
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
+
 
 object MachineActivity {
     private const val serverUrl = "https://34.73.120.24/"
@@ -21,6 +26,7 @@ object MachineActivity {
 
     fun getMachine(label: String?) {
         println("In get machine func")
+        //change request format using jsonobject
         val request = Request.Builder()
             .url(serverUrl + "machine/" + label + "/")
             .build()
@@ -37,8 +43,25 @@ object MachineActivity {
                     machine_name = machineData[0].toString()
                     machine_instructions = machineData[1].toString()
                     machine_url = machineData[2].toString()
+                    val machine = Machine(machine_name, machine_instructions, machine_url)
                 }
             }
         })
     }
+
+    fun getMachineView(context: Context, position: Int, convertView: View?, parent: ViewGroup): View {
+        val machineView = (convertView?.tag /* reuse binding */ ?: run {
+            val rowView = LayoutInflater.from(context).inflate(R.layout.machine_card, parent, false)
+            rowView.tag = MachineCardBinding.bind(rowView) // cache binding
+            rowView.tag
+        }) as MachineCardBinding
+
+        getItem(position)?.run {
+            machineView.machine_name.text = label
+        }
+
+        return machineView.root
+    }
+
+
 }
